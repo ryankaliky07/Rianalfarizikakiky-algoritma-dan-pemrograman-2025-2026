e120c9160a8a5900e7fbb950a34d2cbbc12ac7df
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 05:04:08 +0700

    latihan 1

---
+++ latihan.java/coba1.java
@@ -0,0 +1,13 @@
+public class coba1 {
+    public static void main(String[] args) {
+        String nim = "I.2510235";
+        String nama = "rian alfarizi kaliky";
+        String umur = "24 Tahun";
+        String profesi = "Mahasiswa";
+
+        System.out.println(nim);
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(profesi);
+    }
+}
