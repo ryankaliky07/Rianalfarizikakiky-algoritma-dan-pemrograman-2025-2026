bf35decd6c7a81e7cbd03cae116a09954f619056
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 03:41:10 +0700

    perubahan pada constanta

---
+++ latihan.cpp/constanta.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    const double PI = 3.14159;
+    const int Nilai_Max = 100;
+    const char MyChar = 'A';
+    const string MyName = "rian alfarizi kaliky";
+
+    cout << "Nilai PI = " << PI << endl;
+    cout << "Nilai Nilai_Max = " << Nilai_Max << endl;
+    cout << "Nilai MyChar = " << MyChar << endl;
+    cout << "Nilai MyString = " << MyName << endl;
+    
+    return 0;
+}
