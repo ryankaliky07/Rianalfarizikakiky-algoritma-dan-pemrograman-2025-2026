a2d3764fe295b98cd528433f6c78ef44d22c3c58
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 04:20:37 +0700

    tugas pertemuan ke6

---
+++ latihan.java/bol.java
@@ -0,0 +1,9 @@
+public class bol {
+
+    public static void main(String[] args) { 
+        boolean ApakahDosenAlproKeren = true; 
+        boolean ApakahDosenAlproJelek = false; 
+        System.out.println(ApakahDosenAlproKeren); 
+        System.out.println(ApakahDosenAlproJelek); 
+    }
+}
