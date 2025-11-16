48e3ab55a3c6ba05739807f11873808892802c51
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 03:19:00 +0700

    expresi dan operator ( pertemuan 7 )

---
+++ latihan.cpp/angka.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Masukkan sebuah angka: ";
+    cin >> a;
+    cout << "Masukkan angka kedua: ";
+    cin >> b;
+
+    if (a > b) {
+        cout << a << " adalah angka genap." << b << endl;
+    } else if (a<b)
+    {
+        cout << a << " lebih kecil dari " << b << endl;
+    }
+    else {
+        cout << " Keduanya sama besar" << endl;
+        cout << " Apakah kdeuanya sama?"<< (a==b);
+    }
+}
