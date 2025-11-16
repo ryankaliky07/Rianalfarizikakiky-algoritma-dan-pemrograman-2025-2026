58fc669821f4980df4e83724c38aa2253e7c7611
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 02:41:25 +0700

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,11 @@
+print('---Belajar Variabel---') 
+char = 'B' 
+string = "rian alfarizi kaliky" 
+a = 23
+penghasilan1 = 1000000 
+nilai_uts = 82.25 
+print(char) 
+print(string) 
+print(a) 
+print(penghasilan1) 
+print(nilai_uts) 
