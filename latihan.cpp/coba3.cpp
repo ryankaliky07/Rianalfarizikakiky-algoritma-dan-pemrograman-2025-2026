b69ca468ae7003302d91ae919a1e8a3ff6b9bce5
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 03:36:00 +0700

    pengubaahan nama

---
+++ latihan.cpp/coba3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string nama = "rian alfarizi kaliky";
+    int umur = 23;
+    float tinggi = 170.5;
+    bool aktif = true;
+
+    cout << "Nama : " << nama << endl;
+    cout << "Umur : " << umur << " tahun" << endl;  
+    cout << "Tinggi : " << tinggi << " cm" << endl;
+    cout << "Status Aktif: " << aktif << endl;
+    return 0;
+}
