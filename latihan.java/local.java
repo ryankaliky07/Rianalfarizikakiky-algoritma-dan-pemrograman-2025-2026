0e4011710db3cb3d88dee3488f72de56d80aaae0
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 04:42:32 +0700

    tuugas pertemuan ke 6

---
+++ latihan.java/local.java
@@ -0,0 +1,7 @@
+public class local {
+    
+public static void main(String args[]) { 
+        int age = 23; // local variable 
+        System.out.println("rian alfarizi kaliky : " + age); 
+    }
+}
