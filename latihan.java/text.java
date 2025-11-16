4b737eb8bab6203ea3d52d8ac5fb198828b9dcb2
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 04:44:51 +0700

    tugas pertemuan ke 6

---
+++ latihan.java/text.java
@@ -0,0 +1,6 @@
+public class text {
+    public static void main(String[] args) { 
+        String hello = "Hello, Selamat Datang di Program Studi Ilmu Komputer!!!"; 
+        System.out.println(hello); 
+    }
+}
