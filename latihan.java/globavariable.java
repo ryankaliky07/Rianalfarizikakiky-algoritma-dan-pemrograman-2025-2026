a1d5c70ccf084fe2ea7a8719a3439bcbdfce7fe8
ryankaliky07 <ryankaliky07@gmail.com>
2025-11-17 04:36:00 +0700

    tugas pertemuan ke 6

---
+++ latihan.java/globavariable.java
@@ -0,0 +1,9 @@
+public class globavariable {
+    
+public static int age; // global variable 
+ 
+    public static void main(String args[]) { 
+        age = 23; 
+        System.out.println("rian alfarizi kaliky : " + age); 
+    }
+}
