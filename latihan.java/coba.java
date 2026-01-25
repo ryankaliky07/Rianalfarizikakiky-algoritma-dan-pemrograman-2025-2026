ed80cf9fc336f8ff786fccb28c82322c5cc1bf0e
ryankaliky07 <ryankaliky07@outlook.co.id>
2026-01-26 01:10:10 +0700

    update pertemuan ke 12

---
+++ latihan.java/coba.java
@@ -0,0 +1,12 @@
+class coba {
+public static void main(String[] args) {
+String nim = "I.2510249";
+String nama = "muhammad abdul haqsyist al anwar";
+String umur = "18";
+String profesi = "mahasiswa";
+System.out.println(nim);
+System.out.println(nama);
+System.out.println(umur);
+System.out.println(profesi);
+}
+}
